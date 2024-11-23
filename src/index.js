9bcb6a8f808796c797002e3d1917dc099e0a76a4
VarunSingh-git <varunsaini.a.x@gamil.com>
2024-11-24 01:59:21 +0530

    setup complete

--- src/index.js
+++ src/index.js
@@ -1,37 +1,46 @@
-// require('dotenv').config({ path: './env'})
-import mongoose from "mongoose";
+// we import database connection file from ./db/index.js (this is 1st approach)
+import connectDB from "./db/index.db.js";
+
 import dotenv from "dotenv"
-import express from "express"
-import { DB_NAME } from "./constants.js";
 dotenv.config({
     path: './env'
 }) // when we use import module syntax for dotnv then we should write down config of .env
 
 
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 3000, ()=>{
+        console.log(`Server is running at: ${process.env.PORT}`);
+       app.on("error",(err)=>{
+console.log('This is ERROR:',err);
+
+       }) 
+    })
+})
+.catch((err)=>console.log(err));
+
+
+
+
+
+
+
 // here we write our code directly (this is 2nd approach)
-const app = express()
+// const app = express()
 
-;(async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("error",(err)=>{
-            console.log(`ERROR: ${err}`);
-            throw err
-        })
+// ;(async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(err)=>{
+//             console.log(`ERROR: ${err}`);
+//             throw err
+//         })
         
-        app.listen(process.env.PORT, ()=>{
-            console.log(`App is listening on ${process.env.PORT}`);
-        })
-    } catch (error) {
-        console.log(`ERROR: ${error}`);
-        throw error
-    }
-})()
-
-
-
-// we import database connection file from ./db/index.js (this is 1st approach)
-/*import connectDB from "./db/index.js";
-
-
-connectDB()*/ 
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`App is listening on ${process.env.PORT}`);
+//         })
+//     } catch (error) {
+//         console.log(`ERROR: ${error}`);
+//         throw error
+//     }
+// })()
