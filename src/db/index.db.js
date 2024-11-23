3b7086de80d8b1b6d974b4f792358fed440a03a5
VarunSingh-git <varunsaini.a.x@gamil.com>
2024-11-24 03:06:19 +0530

    connect to mongo db

--- src/db/index.db.js
+++ src/db/index.db.js
@@ -7,8 +7,8 @@ dotenv.config({
 
 const connectDB = async () => {
     try {
-        const connection = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`) // connection study
-        console.log(`\n MongoDB connect !! DB Host ${connection.connection.host}`); // study
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`) // connection study
+        console.log(`\n MongoDB connect !! DB Host ${connectionInstance.connection.host}`); // study
     } catch (error) {
         console.log(`ERROR: ${error}`);
         process.exit(1) // study 
@@ -16,3 +16,25 @@ const connectDB = async () => {
 }
 
 export default connectDB
+
+
+
+// here we write our code directly (this is 2nd approach)
+// const app = express()
+
+// ;(async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(err)=>{
+//             console.log(`ERROR: ${err}`);
+//             throw err
+//         })
+        
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`App is listening on ${process.env.PORT}`);
+//         })
+//     } catch (error) {
+//         console.log(`ERROR: ${error}`);
+//         throw error
+//     }
+// })()
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // we import database connection file from ./db/index.js (this is 1st approach)
 import connectDB from "./db/index.db.js";
-
+import express from "express"
+const app =express()
 import dotenv from "dotenv"
 dotenv.config({
     path: './env'
@@ -11,36 +12,10 @@ connectDB()
 .then(()=>{
     app.listen(process.env.PORT || 3000, ()=>{
         console.log(`Server is running at: ${process.env.PORT}`);
-       app.on("error",(err)=>{
-console.log('This is ERROR:',err);
+            app.on("error",(err)=>{
+                console.log('This is ERROR:',err);
 
        }) 
     })
 })
 .catch((err)=>console.log(err));
-
-
-
-
-
-
-
-// here we write our code directly (this is 2nd approach)
-// const app = express()
-
-// ;(async () => {
-//     try {
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//         app.on("error",(err)=>{
-//             console.log(`ERROR: ${err}`);
-//             throw err
-//         })
-        
-//         app.listen(process.env.PORT, ()=>{
-//             console.log(`App is listening on ${process.env.PORT}`);
-//         })
-//     } catch (error) {
-//         console.log(`ERROR: ${error}`);
-//         throw error
-//     }
-// })()
